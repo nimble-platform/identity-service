df46fc30c3addad22c3c0f6665ad2984c2cb34a5
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-03-23 08:39:00 +0100

    worked on api specs

--- identity-service/src/main/java/eu/nimble/service/identity/model/Company.java
+++ identity-service/src/main/java/eu/nimble/service/identity/model/Company.java
@@ -1,14 +1,6 @@
 package eu.nimble.service.identity.model;
 
-/**
- * Created by jinnerbi on 22/03/17.
- */
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
