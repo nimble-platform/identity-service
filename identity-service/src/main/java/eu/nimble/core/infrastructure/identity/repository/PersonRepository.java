d103508f1e675dbb4cb82213ebfee1fd52a0db08
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-09-27 13:18:15 +0200

    test commit

--- identity-service/src/main/java/eu/nimble/core/infrastructure/identity/repository/PersonRepository.java
+++ identity-service/src/main/java/eu/nimble/core/infrastructure/identity/repository/PersonRepository.java
@@ -3,6 +3,7 @@ package eu.nimble.core.infrastructure.identity.repository;
 import eu.nimble.service.model.ubl.commonaggregatecomponents.PersonType;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.annotation.Secured;
 
 import java.util.List;
 
