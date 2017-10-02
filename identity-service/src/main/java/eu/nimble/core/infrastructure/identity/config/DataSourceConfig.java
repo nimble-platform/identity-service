ffd9d1dc751748abdaa75b9b2cf79989c6b9f10b
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-10-02 18:27:12 +0200

    fixed typo

--- identity-service/src/main/java/eu/nimble/core/infrastructure/identity/config/DataSourceConfig.java
+++ identity-service/src/main/java/eu/nimble/core/infrastructure/identity/config/DataSourceConfig.java
@@ -17,7 +17,7 @@ import java.util.regex.Pattern;
 
 @Component
 @Configuration
-@Profile("kubernetesf")
+@Profile("kubernetes")
 @SuppressWarnings({"unused", "ResultOfMethodCallIgnored"})
 public class DataSourceConfig {
 
