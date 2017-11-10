eb9da3933e830b122a1cd7a8dff56616f1bc98b9
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-11-10 09:36:29 +0100

    worked on proper endpoints

--- identity-service/src/main/java/eu/nimble/core/infrastructure/identity/IdentityServiceApplication.java
+++ identity-service/src/main/java/eu/nimble/core/infrastructure/identity/IdentityServiceApplication.java
@@ -1,10 +1,12 @@
 package eu.nimble.core.infrastructure.identity;
 
 import eu.nimble.core.infrastructure.identity.entity.UaaUser;
+import eu.nimble.core.infrastructure.identity.utils.mail.EmailService;
 import eu.nimble.service.model.ubl.commonaggregatecomponents.ActivityDataLineType;
 import eu.nimble.service.model.ubl.commonaggregatecomponents.PartyType;
 import eu.nimble.service.model.ubl.commonaggregatecomponents.PersonType;
 import eu.nimble.service.model.ubl.commonbasiccomponents.CodeType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -33,12 +35,18 @@ public class IdentityServiceApplication extends SpringBootServletInitializer {
     @Value("${nimble.cors_enabled}")
     private String corsEnabled;
 
+    @Autowired
+    EmailService emailService;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+
+        emailService.sendInvite("johannes.innerbichler@salzburgresearch.at");
+
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                if( corsEnabled.equals("true"))
+                if (corsEnabled.equals("true"))
                     registry.addMapping("/**").allowedOrigins("*");
             }
         };
