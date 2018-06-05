73e9c4df8f045ca2eb63e4cd6d7efbd9f9e1471e
Johannes Innerbichler <j.innerbichler@gmail.com>
2018-06-05 16:36:20 +0200

    added data for into endpoint

--- identity-service/src/main/java/eu/nimble/core/infrastructure/identity/controller/frontend/UserIdentityController.java
+++ identity-service/src/main/java/eu/nimble/core/infrastructure/identity/controller/frontend/UserIdentityController.java
@@ -32,13 +32,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.resource.OAuth2AccessDeniedException;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.WebApplicationException;
 import java.io.IOException;
 import java.util.HashMap;
