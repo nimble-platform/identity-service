97265cf40f3a96ec75d10117d62b677f0506de6c
Johannes Innerbichler <j.innerbichler@gmail.com>
2018-06-20 16:00:32 +0200

    added endpoint for UBL compliant parties

--- identity-service/src/main/java/eu/nimble/core/infrastructure/identity/controller/ubl/PartyController.java
+++ identity-service/src/main/java/eu/nimble/core/infrastructure/identity/controller/ubl/PartyController.java
@@ -1,5 +1,7 @@
 package eu.nimble.core.infrastructure.identity.controller.ubl;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import eu.nimble.core.infrastructure.identity.controller.IdentityUtils;
 import eu.nimble.core.infrastructure.identity.repository.PartyRepository;
 import eu.nimble.core.infrastructure.identity.repository.PersonRepository;
@@ -12,7 +14,9 @@ import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +24,13 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.namespace.QName;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -115,5 +125,51 @@ public class PartyController {
         List<PartyType> parties = partyRepository.findByPerson(person);
 
         return new ResponseEntity<>(parties, HttpStatus.OK);
+    }
+
+    @SuppressWarnings("PointlessBooleanExpression")
+    @ApiOperation(value = "", notes = "Get Party for Id in the UBL format.", response = PartyType.class, tags = {})
+    @RequestMapping(value = "/party/ubl/{partyId}", produces = {"text/xml"}, method = RequestMethod.GET)
+    ResponseEntity<String> getPartyUbl(
+            @ApiParam(value = "Id of party to retrieve.", required = true) @PathVariable Long partyId,
+            @RequestHeader(value = "Authorization") String bearer) throws IOException, JAXBException {
+
+        // search relevant parties
+        List<PartyType> parties = partyRepository.findByHjid(partyId);
+
+        // check if party was found
+        if (parties.isEmpty()) {
+            logger.info("Requested party with Id {} not found", partyId);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        PartyType party = parties.get(0);
+
+        // remove person depending on access rights
+        if (identityUtils.hasRole(bearer, OAuthClient.Role.LEGAL_REPRESENTATIVE) == false)
+            party.setPerson(new ArrayList<>());
+
+        StringWriter serializedCatalogueWriter = new StringWriter();
+        String packageName = party.getClass().getPackage().getName();
+        JAXBContext jc = JAXBContext.newInstance(packageName);
+
+        XmlMapper xmlMapper = new XmlMapper();
+        xmlMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
+        Marshaller marsh = jc.createMarshaller();
+        marsh.setProperty("jaxb.formatted.output", true);
+        marsh.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        @SuppressWarnings("unchecked")
+        JAXBElement element = new JAXBElement(new QName("urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2", "Party"), party.getClass(), party);
+        marsh.marshal(element, serializedCatalogueWriter);
+
+        // log the catalogue to be transformed
+        String xmlParty = serializedCatalogueWriter.toString();
+        xmlParty = xmlParty.replaceAll(" Hjid=\"[0-9]+\"", "");
+        serializedCatalogueWriter.flush();
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setContentType(MediaType.TEXT_XML);
+        return new ResponseEntity<>(xmlParty, responseHeaders, HttpStatus.OK);
     }
 }
