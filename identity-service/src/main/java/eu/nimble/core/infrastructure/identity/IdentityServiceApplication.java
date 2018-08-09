e638f98df9d309281cc73b2efcf914bb74aae6d4
Johannes Innerbichler <j.innerbichler@gmail.com>
2018-08-09 13:47:23 +0200

    updated commons dependency

--- identity-service/src/main/java/eu/nimble/core/infrastructure/identity/IdentityServiceApplication.java
+++ identity-service/src/main/java/eu/nimble/core/infrastructure/identity/IdentityServiceApplication.java
@@ -7,14 +7,12 @@ import eu.nimble.service.model.ubl.commonbasiccomponents.CodeType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,12 +20,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 import java.net.URISyntaxException;
 
-@ComponentScan
 @Configuration
 @EnableDiscoveryClient
 @SpringBootApplication
 @EntityScan(basePackageClasses = {UaaUser.class, PartyType.class, PersonType.class, CodeType.class})
-@EnableAutoConfiguration
 public class IdentityServiceApplication extends SpringBootServletInitializer {
 
     private static final Logger logger = LoggerFactory.getLogger(IdentityServiceApplication.class);
