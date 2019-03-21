2ee0707d553687a63ec83e08e44156597c0f89c7
sshniro <sshniro@gmail.com>
2019-03-21 17:42:13 +0100

    Fix for the invite list not shown in the company invitation

--- identity-service/src/main/java/eu/nimble/core/infrastructure/identity/system/InvitationController.java
+++ identity-service/src/main/java/eu/nimble/core/infrastructure/identity/system/InvitationController.java
@@ -93,6 +93,8 @@ public class InvitationController {
         List<String> userRoleIDs = invitation.getRoleIDs() == null ? new ArrayList() : invitation.getRoleIDs();
         List<String> prettifedRoles = KeycloakAdmin.prettfiyRoleIDs(userRoleIDs);
 
+        UserInvitation userInvitation = new UserInvitation(emailInvitee, companyId, userRoleIDs, sender);
+
         // check if user is already registered
         Optional<UaaUser> potentialInvitee = uaaUserRepository.findByUsername(emailInvitee).stream().findFirst();
         if (potentialInvitee.isPresent()) {
@@ -106,9 +108,6 @@ public class InvitationController {
                 return new ResponseEntity<>(HttpStatus.CONFLICT);
             }
 
-            // saving invitation
-            UserInvitation userInvitation = new UserInvitation(emailInvitee, companyId, userRoleIDs, sender);
-            userInvitationRepository.save(userInvitation);
 
             // ToDo: let user accept invitation
 
@@ -132,6 +131,9 @@ public class InvitationController {
             response.put("message", "Existing user added to company");
             return new ResponseEntity<>(response, HttpStatus.CREATED);
         }
+
+        // saving invitation
+        userInvitationRepository.save(userInvitation);
 
         // send invitation
         String companyName = UblUtils.getName(company.getPartyName(), NimbleConfigurationProperties.LanguageID.ENGLISH);
