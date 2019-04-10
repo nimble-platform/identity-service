73c8c8feb7460069c079eba97358e007aff98f8e
Akayeshmantha <akayeshmantha@apache.org>
2019-04-10 14:32:44 +0200

    Swagger host fix.

--- identity-service/src/main/java/eu/nimble/core/infrastructure/identity/config/SwaggerConfig.java
+++ identity-service/src/main/java/eu/nimble/core/infrastructure/identity/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package eu.nimble.core.infrastructure.identity.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -13,9 +14,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+
+    @Value("${nimble.platformHost}")
+    private String platformHost;
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .host(platformHost)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("eu.nimble.core"))
                 .paths(PathSelectors.any())
