05cfea34dd0dde94ab4d804988da61c7bc499ac4
sshniro <sshniro@gmail.com>
2019-04-17 15:21:18 +0200

    Test case modifications for the ubl data mode change in (industry sectors, company statement)

--- identity-service/src/test/java/eu/nimble/core/infrastructure/identity/TestUtils.java
+++ identity-service/src/test/java/eu/nimble/core/infrastructure/identity/TestUtils.java
@@ -1,9 +1,11 @@
 package eu.nimble.core.infrastructure.identity;
 
+import eu.nimble.core.infrastructure.identity.config.NimbleConfigurationProperties;
 import eu.nimble.core.infrastructure.identity.entity.dto.*;
 import eu.nimble.service.model.ubl.commonaggregatecomponents.PersonType;
 
 import java.util.Collections;
+import java.util.HashMap;
 
 import static eu.nimble.core.infrastructure.identity.config.NimbleConfigurationProperties.LanguageID.ENGLISH;
 
@@ -16,11 +18,17 @@ public class TestUtils {
 
     public static CompanyRegistration createCompanyRegistration(String legalName, PersonType user) {
 
+        HashMap<NimbleConfigurationProperties.LanguageID, String> industrySectors = new HashMap<>();
+        industrySectors.put(ENGLISH, "industrySector");
+
+        HashMap<NimbleConfigurationProperties.LanguageID, String> companyStatement = new HashMap<>();
+        companyStatement.put(ENGLISH, "industrySector");
+
         CompanyDetails companyDetails = new CompanyDetails(Collections.singletonMap(ENGLISH, "brand name"),
                 Collections.singletonMap(ENGLISH, legalName), "vat number", "verification info",
                 new Address(), "business type", Collections.singletonMap(ENGLISH, "business type"), 1970,
-                Collections.singletonList("industry sector"));
-        CompanyDescription companyDescription = new CompanyDescription("statement", "website",
+                industrySectors);
+        CompanyDescription companyDescription = new CompanyDescription(companyStatement, "website",
                 Collections.singletonList("photos"), "imageId", Collections.singletonList("social media"),
                 Collections.singletonList(new CompanyEvent()), Collections.singletonList("test"));
         CompanyTradeDetails companyTradeDetails = new CompanyTradeDetails();
