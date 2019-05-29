57bca9e627c692586e6851003ce525b12a12f056
Akayeshmantha <akayeshmantha@apache.org>
2019-05-29 22:32:13 +0200

    adding party as a json field.

--- identity-service/src/main/java/eu/nimble/core/infrastructure/identity/entity/NegotiationSettings.java
+++ identity-service/src/main/java/eu/nimble/core/infrastructure/identity/entity/NegotiationSettings.java
@@ -25,7 +25,6 @@ public class NegotiationSettings implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @JsonIgnore
     @OneToOne(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
     @JoinColumn(unique = true)
     private PartyType company;
