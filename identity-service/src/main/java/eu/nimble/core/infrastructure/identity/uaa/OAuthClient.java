73e1c4b49cd593e1e4b102326de14d2972faf625
Akayeshmantha <akayeshmantha@apache.org>
2019-09-16 12:28:50 +0200

    adapt invitation controller for the new nimble deleted role.

--- identity-service/src/main/java/eu/nimble/core/infrastructure/identity/uaa/OAuthClient.java
+++ identity-service/src/main/java/eu/nimble/core/infrastructure/identity/uaa/OAuthClient.java
@@ -67,7 +67,8 @@ public class OAuthClient {
         LEGAL_REPRESENTATIVE("legal_representative"),
         PLATFORM_MANAGER("platform_manager"),
         COMPANY_ADMIN("company_admin"),
-        PUBLISHER("publisher");
+        PUBLISHER("publisher"),
+        NIMBLE_DELETED_USER("nimble_deleted_user");
 
         private final String role;
 
