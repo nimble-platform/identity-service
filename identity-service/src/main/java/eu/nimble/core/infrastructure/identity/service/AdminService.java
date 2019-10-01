e0b6fbb665488b244a9ef2cef3990e021a332b9a
Akayeshmantha <akayeshmantha@apache.org>
2019-10-01 11:11:50 +0200

    change query company endpoint.

--- identity-service/src/main/java/eu/nimble/core/infrastructure/identity/service/AdminService.java
+++ identity-service/src/main/java/eu/nimble/core/infrastructure/identity/service/AdminService.java
@@ -68,7 +68,7 @@ public class AdminService {
     //    @Cacheable("unverifiedCompanies")
     public List<PartyType> queryCompanies(CompanyState companyState) {
         List<PartyType> resultingCompanies = new ArrayList<>();
-        Iterable<PartyType> allParties = partyRepository.findAll(new Sort(Sort.Direction.ASC, "hjid"));
+        Iterable<PartyType> allParties = partyRepository.findAllByDeletedIsFalse(new Sort(Sort.Direction.ASC, "hjid"));
         for (PartyType company : allParties) {
 
             // collect roles of company members
